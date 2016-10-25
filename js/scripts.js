c93c58979e222fd31fac2d3dd799c25874a116d0
Chris Wingo and Nigel Burnett <nigel@>
2016-10-25 16:02:40 -0700

    Further separated business logic / UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,8 @@ $(document).ready(function() {
     var checkboxName = "";
     var luckyEvents = getCheckboxInput("lucky-events");
     var unluckyEvents = getCheckboxInput("unlucky-events");
-
-    if(luckyEvents.length > 4 && unluckyEvents.length < 3){
-      $("#fortune p").text("You are an extremely lucky person. I see nothing but good things in your future. Go play the lottery today!");
-    } else if (luckyEvents.length < 3 && unluckyEvents.length >= 4){
-      $("#fortune p").text("You are an extremely unlucky person. I see doom and gloom in your future. Go hide somewhere until your luck changes!");
-    } else if (luckyEvents.length < 4 && unluckyEvents.length > 3){
-      $("#fortune p").text("You are in state of luck fluxuation. I cannot see what lies ahead for you. Be careful of the next move you take.");
-    } else {
-      $("#fortune p").text("Chaos swirls around you. My crystal ball can't get any good reading for your future. Please try again later.");
-    }
+    
+    tellFortune(luckyEvents.length, unluckyEvents.length);
   });
 });
 
@@ -26,7 +18,19 @@ function getCheckboxInput(checkboxName){
     var selection = $(this).val();
     inputs.push(selection);
   });
-
   return inputs;
-
 };
+
+function tellFortune(luckyNumber, unluckyNumber) {
+
+  if(luckyNumber > 4 && unluckyNumber < 3){
+    $("#fortune p").text("You are an extremely lucky person. I see nothing but good things in your future. Go play the lottery today!");
+  } else if (luckyNumber < 3 && unluckyNumber >= 4){
+    $("#fortune p").text("You are an extremely unlucky person. I see doom and gloom in your future. Go hide somewhere until your luck changes!");
+  } else if (luckyNumber < 4 && unluckyNumber > 3){
+    $("#fortune p").text("You are in state of luck fluxuation. I cannot see what lies ahead for you. Be careful of the next move you take.");
+  } else {
+    $("#fortune p").text("Chaos swirls around you. My crystal ball can't get any good reading for your future. Please try again later.");
+  }
+
+}
